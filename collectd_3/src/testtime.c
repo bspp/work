520f61adac9c6286e5853c29a8a9730fa5bbab64
linyp <linyp@lenovocloud.com>
2018-04-27 08:09:29 -0400

    collectd

--- collectd_3/src/testtime.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(){
-	char buffer[20]="1505723380";
-	char *stop;
-	printf("%ld\n",strtol(buffer, &stop, 10));
-	printf("%s\n", stop);
-}
--- collectd_3/src/types_list.h
+++
@@ -1,4 +0,0 @@
-#ifndef TYPES_LIST_H
-#define TYPES_LIST_H
-int read_types_list(const char *file);
-#endif
